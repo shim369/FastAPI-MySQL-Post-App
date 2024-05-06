5247dae3b364418534ab177b04ba09a1fa6c0f89
Shimpei Ohtani <ohtani.shimpei@gmail.com>
2024-05-06 17:20:31 +0900

    update models.py

--- models.py
+++ models.py
@@ -1,16 +1,9 @@
 from sqlalchemy import Column, Integer, String
 from database import Base
 
-class User(Base):
-    __tablename__ = 'users'
-
-    id = Column(Integer, primary_key=True, index=True)
-    username = Column(String(50), unique=True)
-
 class Post(Base):
     __tablename__ = 'posts'
 
     id = Column(Integer, primary_key=True, index=True)
     title = Column(String(200))
     url = Column(String(200))
-    user_id = Column(Integer)
